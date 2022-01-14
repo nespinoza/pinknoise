b4aa7a0a277e8c114408e199dce4db798784f2e1
Nestor Espinoza <nespinoza@stsci.edu>
2022-01-14 17:07:11 -0500

    pinknoise setup works

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import re
-from setuptools import setup
+import numpy
+#from distutils.core import setup, Extension
+from setuptools import setup, Extension
 
-VERSIONFILE='pinknoise/_version.py'
+VERSIONFILE='src/pinknoise/_version.py'
 verstrline = open(VERSIONFILE, "rt").read()
 VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
 mo = re.search(VSRE, verstrline, re.M)
@@ -10,6 +12,7 @@ if mo:
 else:
     raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
 
+module = Extension('FWT', sources = ['src/pinknoise/FWT.c'], libraries=['m'], include_dirs=[numpy.get_include(),'/usr/local/include'])
 setup(name='pinknoise',
       version=verstr,
       description='pinknoise: a library to perform inference and simulations of all flavors of pink noise',
@@ -18,9 +21,8 @@ setup(name='pinknoise',
       author_email='nespinoza@stsci.edu',
       license='MIT',
       packages=['pinknoise'],
+      package_dir={'pinknoise': 'src/pinknoise'},
       install_requires=['numpy','scipy'],
       python_requires='>=3.0',
-      extras_requires={
-            'seaborn':['seaborn'],
-            'matplotlib':['matplotlib'],},
+      ext_modules = [module],
       zip_safe=False)
--- src/pinknoise/compute.py
+++ src/pinknoise/compute.py
@@ -1,5 +1,7 @@
+import numpy as np
 from .utils import *
 
+log2pi = np.log(2.*np.pi)
 class compute(object):
     """
     This class perform the main computations for our time-series.
@@ -8,9 +10,12 @@ class compute(object):
         Number of datapoints in the time-series. This is used to zero-pad the time-series. Note the time-series is expected to have constant sampling. 
     """
     def normal_like(self, x, mu, tau):
+        """
+        Normal log-likelihood:
+        """
         return 0.5*(np.log(tau) - log2pi - tau*( (x-mu)**2))
 
-    def get_fn_likelihood(self, residuals, sigma_w, sigma_r, gamma=1.0):
+    def get_likelihood(self, residuals, sigma_w, sigma_r, gamma=1.0):
 
         like=0.0
 
@@ -25,23 +30,32 @@ class compute(object):
            g_gamma=1.0/(2.0*np.log(2.0))  # (value assuming gamma=1)
         else:
            g_gamma=(2.0)-(2.0)**gamma
-        # log-Likelihood of the aproximation coefficients
+
+        # Now we calculate the log-Likelihood using equation (32) in Carter & Winn. First, the sigma given by eq. (34):
         sigmasq_S=(sigma_r**2)*g_gamma+(sigma_w)**2
         tau_a =  1.0/sigmasq_S
-        like += normal_like( bb[0], 0.0 , tau_a )
-        k=long(0)
+    
+        # The trick is that the FWT code sets n0 in the notation of Carter & Winn to 1 (i.e., the code only works if we have 
+        # datasets of size N = n0 * 2**M). So, with this, we only have to compute one term for the second product in eq. (32) 
+        # (the one that contains sigma_S^2, sigmasq_S above):
+        like += self.normal_like( bb[0], 0.0 , tau_a )
+
+        # Now we have to handle the first term in the product. We compute it sequentially (TODO: this probably can be moved 
+        # to a C function to speed up the likelihood evaluation). We iterate over all the possible m's in equation (32); 
+        # note ii is *not* m:
+        k = 0
         SS=range(self.M)
         for ii in SS:
-                # log-Likelihood of the detail coefficients with m=i...
+                # We first do the m = 2. Here n goes from 1 to n0*2 = 2.  
                 if(ii==0):
                   sigmasq_W=(sigma_r**2)*(2.0**(-gamma*np.double(1.0)))+(sigma_w)**2
                   tau=1.0/sigmasq_W
-                  like += normal_like( bb[1], 0.0, tau )
+                  like += self.normal_like( bb[1], 0.0, tau )
                 else:
                   sigmasq_W=(sigma_r**2)*(2.0**(-gamma*np.double(ii+1)))+(sigma_w)**2
                   tau=1.0/sigmasq_W
                   for j in range(2**ii):
-                      like += normal_like( aa[k], 0.0 , tau )
+                      like += self.normal_like( aa[k], 0.0 , tau )
                       k=k+1
         return like
 
@@ -54,12 +68,13 @@ class compute(object):
 
         # Set the wavelet being used:
         if (wavelet == 'daub4'):
+            # Coefficients are taken from eq. 13.10.5 in Numerical Recipes:
             self.wavelet = 'daub4'
             self.C = np.double(np.arange(4))
             self.C[0] = abs((1.0+sqrt(3.0))/(4.0*sqrt(2.0)))
             self.C[1] = abs((3.0+sqrt(3.0))/(4.0*sqrt(2.0)))
             self.C[2] = abs((3.0-sqrt(3.0))/(4.0*sqrt(2.0)))
-            self.C[3] = -abs((1.0-sqrt(3.0))/(4.0*sqrt(2.0)))
+            self.C[3] = abs((1.0-sqrt(3.0))/(4.0*sqrt(2.0)))
             self.nC = 4
 
 
